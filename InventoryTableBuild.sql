fb91d9c89e8c3adcb3ea4c83162c800fe16483b7
Steven Cyrway <stevencyrway@gmail.com>
2021-09-16 16:55:03 -0500

    table is final

--- InventoryTableBuild.sql
+++ InventoryTableBuild.sql
@@ -1,22 +1,44 @@
 use warehouse COMPUTE_WH;
-use database FIVETRAN_DB;
+use database PROD_TLA_DW
+
+--To create the table
+CREATE OR REPLACE TABLE PROD_TLA_DW.public.INVENTORY_FACT
+(
+    DATE       DATE          NOT NULL,
+    SKU        VARCHAR(255)  NULL,
+    UUID       VARCHAR(255)  NULL,
+    QOH        SMALLINT      NULL,
+    BACKORDER  SMALLINT      NULL,
+    COST       DECIMAL(6, 2) NULL,
+    ITEMID     VARCHAR(50)   NULL,
+    LOCATIONID VARCHAR(50)   Null,
+    CATEGORYID VARCHAR(100)  NULL,
+    SOURCE     VARCHAR(100)  NULL
+)
+
+As
 
 --Inventory Table Build
+-- !! Need to confirm timezones of each data that's landed to ensure proper time zone offset.
 WITH RECURSIVE
-     ctelightspeedItem AS (Select id,
-                                  CATEGORY_ID,
-                                 to_date(UPDATED_TIME) as ItemDate,
-                                  CUSTOM_SKU,
-                                  ROW_NUMBER() OVER (PARTITION BY ID ORDER BY UPDATED_TIME DESC) as RowNumber
-                           FROM LIGHT_SPEED_RETAIL.ITEM_HISTORY)
-   --Assigns row numbers to to get values over time
-   , cteLightspeedInventory AS (Select to_date(UPDATED_TIME) as InventoryDate,
-                             item_id,
-                             shop_id,
-                             qoh,
-                             backorder,
-                             ROW_NUMBER() OVER (PARTITION BY ID, ITEM_ID, SHOP_ID, to_date(UPDATED_TIME) ORDER BY UPDATED_TIME DESC) as RowNumber
-                      FROM LIGHT_SPEED_RETAIL.ITEM_SHOP_HISTORY)
+---- /// Lightspeed /// ----
+   -- Assigns row numbers to to get values over time and identify when more than value occurs in a day.
+    ctelightspeedItem AS (Select id,
+                                 CATEGORY_ID,
+                                 to_date(UPDATED_TIME)                                          as ItemDate,
+                                 CUSTOM_SKU,
+                                 AVG_COST,
+                                 ROW_NUMBER() OVER (PARTITION BY ID ORDER BY UPDATED_TIME DESC) as RowNumber
+                          FROM FIVETRAN_DB.LIGHT_SPEED_RETAIL.ITEM_HISTORY)
+   -- Assigns row numbers to to get values over time
+   , cteLightspeedInventory AS (Select to_date(UPDATED_TIME)                                                                                   as InventoryDate,
+                                       item_id,
+                                       shop_id,
+                                       qoh,
+                                       backorder,
+                                       ROW_NUMBER() OVER (PARTITION BY ID, ITEM_ID, SHOP_ID, to_date(UPDATED_TIME) ORDER BY UPDATED_TIME DESC) as RowNumber
+                                FROM FIVETRAN_DB.LIGHT_SPEED_RETAIL.ITEM_SHOP_HISTORY)
+   -- this table combines the two above and then filters to only rownumber 1 to get the Most recent value in a given day.
    , ctelightspeedcombined as (Select item_id,
                                       shop_id,
                                       qoh,
@@ -24,100 +46,84 @@ WITH RECURSIVE
                                       cteLightspeedInventory.rownumber as RN,
                                       id,
                                       category_id,
-                                      InventoryDate as Date,
-                                      custom_sku
+                                      InventoryDate                    as Date,
+                                      custom_sku,
+                                      AVG_COST as Cost
                                from cteLightspeedInventory
                                         left outer join ctelightspeedItem
                                                         on cteLightspeedInventory.ITEM_ID = ctelightspeedItem.ID
-                                                            where ctelightspeedItem.RowNumber = 1
-                                                            and cteLightspeedInventory.RowNumber = 1
-)
-   --Preps shopify by joining product and product variant to get all details
-   , cteShopifyinventory AS (Select 'Shopify'              as Source,
-                                    P.product_type         as Category1,
-                                    NULL                   as Category2,
-                                    NULL                   as Category3,
-                                    TO_DATE(il.UPDATED_AT) as Date,
-                                    II.COST                as default_cost,
-                                    PV.Sku,
-                                    P.status,
-                                    IL.available           as qoh,
-                                    NULL                   as backorder,
-                                    NULL                   as reorder_point,
-                                    NULL                   as reorder_level,
-                                    'Shopify Ecom'         as ShopName,
-                                    NULL                   as TIME_ZONE,
-                                    1                      as RN
-                             from LOVERS_SHOPIFY.PRODUCT as P
-                                      join LOVERS_SHOPIFY.PRODUCT_VARIANT as PV
-                                           on P.ID = PV.PRODUCT_ID
-                                      join LOVERS_SHOPIFY.INVENTORY_ITEM as II
-                                           on pv.INVENTORY_ITEM_ID = ii.ID
-                                      join LOVERS_SHOPIFY.INVENTORY_LEVEL IL
-                                           on PV.INVENTORY_ITEM_ID = IL.INVENTORY_ITEM_ID)
-   --Assigns row numbers to multiple inventory moves per day
+                               where ctelightspeedItem.RowNumber = 1
+                                 and cteLightspeedInventory.RowNumber = 1)
+---- /// YANDY /// ----
+   -- Assigns row numbers to multiple inventory moves per day
+   , cteyandycategoryjoin as (Select PROD_ID,
+                                     PRODUCTS.ptype,
+                                     PRODUCTS.n2,
+                                     LOCATION,
+                                     concat(PTYPE, '_', N2) as CategoryID
+                              from FIVETRAN_DB.POSTGRES_PUBLIC.PRODUCTS)
    , cteYandyInventoryHistoryPrep AS (Select im.oldinv,
-                                         to_date(im.changetime) as InventoryDate,
-                                         im.option_id,
-                                         im.newinv as QOH,
-                                             ROW_NUMBER() OVER (PARTITION BY OPTION_ID,to_date(CHANGETIME) ORDER BY CHANGETIME DESC) as RowNumber
-                                  from POSTGRES_PUBLIC.INVENTORY_MOVE IM)
-   ---!! neeed clarity !! Not sure which price we should be using from product options in yandy, there exists catalog price, map price, option price and more.
+                                             to_date(im.changetime)                                                                  as InventoryDate,
+                                             im.option_id,
+                                             im.newinv                                                                               as QOH,
+                                             ROW_NUMBER() OVER (PARTITION BY OPTION_ID,to_date(CHANGETIME) ORDER BY CHANGETIME DESC) as InventoryRowNumber
+                                      from FIVETRAN_DB.POSTGRES_PUBLIC.INVENTORY_MOVE IM)
    , cteYandyProducts AS (select po.PROD_ID as UUID,
                                  PROD_ID,
                                  PROD_OPTION_ID,
-                                 po.option_sku,
-                                 po.option_price,
-                                 null       as Cost
-                          from POSTGRES_PUBLIC.product_options po)
-   , cteyandyinventorycombined as (Select oldinv,
-                                          inventorydate,
+                                 po.option_sku
+                          from FIVETRAN_DB.POSTGRES_PUBLIC.product_options po)
+   , cteyandyavgcost as (select FIFO_LEDGER.prod_option_id,
+                                avg(FIFO_LEDGER.cost_price) as Cost
+                         from FIVETRAN_DB.POSTGRES_PUBLIC.FIFO_LEDGER
+                         group by PROD_OPTION_ID)
+   , cteyandyinventorycombined as (Select inventorydate,
                                           option_id,
                                           qoh,
-                                          rownumber,
+                                          InventoryRowNumber,
                                           uuid,
-                                          prod_id,
-                                          prod_option_id,
+                                          cteYandyProducts.prod_id,
+                                          cteYandyProducts.prod_option_id,
+                                          CategoryID,
                                           option_sku,
-                                          option_price,
-                                          cost
+                                          cteyandyavgcost.Cost
                                    from cteYandyInventoryHistoryPrep
                                             join cteYandyProducts on PROD_OPTION_ID = OPTION_ID
-                                   where RowNumber = 1)
+                                            join cteyandyavgcost on cteyandyavgcost.PROD_OPTION_ID = cteYandyProducts.PROD_OPTION_ID
+                                            join cteyandycategoryjoin on cteYandyProducts.PROD_ID = cteyandycategoryjoin.PROD_ID
+                                   where InventoryRowNumber = 1)
 
---Lightspeed Completed Inventory Fact Details
-Select item_id,
-       shop_id,
-       qoh,
+
+---//// WHERE IT ALL COMBINES ////---
+-- the intent here is to take all the relevant cte's above and combine them through unions to make one conjoined inventory table.
+-- This methodology will be followed for all tables.
+
+-- First attempt at Yandy Inventory table, need to add cost next from FIFO ledger
+Select inventorydate            as Date,
+       option_sku               as SKU,
+       concat('Yandy', to_varchar(UUID)) as UUID,
+       QOH,
+       Null                     as Backordered,
+       Cost,
+       prod_option_id           as ItemID,
+       null                     as LocationID, --couldn't find shop values in yandy data, need to ask Aras.
+       CategoryID               as CategoryID,
+       'Yandy'                  as Source
+from cteyandyinventorycombined
+
+union all
+
+-- Lightspeed Completed Inventory Fact Details, this is missing cost.
+Select Date,
+       custom_sku                  as SKU,
+       concat(to_varchar(ITEM_ID), 'LoversLightspeed') as UUID,
+       QOH,
        backorder,
-       id,
-       category_id,
-       Date,
-       custom_sku
-from ctelightspeedcombined
-where RN = 1
-and ITEM_ID = 2407
-
-
-
----Need Yandy location data for location id
-Select YIHP.oldinv,
-       YIHP.InventoryDate,
-       YIHP.option_id,
-       YIHP.newinv,
-       po.OPTION_INV,
-       po.CATALOG_PRICE,
-       po.ON_ORDER,
-       po.BACKORDERED
-from cteYandyInventoryHistoryPrep YIHP
-         join POSTGRES_PUBLIC.PRODUCT_OPTIONS PO
-              on po.PROD_OPTION_ID = YIHP.OPTION_ID
-                  and YIHP.RowNumber = 1;
-
-
-
-
-
-
+       Cost                        as Cost, --Need to add this in upstream table
+       item_id                     as ItemID,
+       shop_id                     as LocationID,
+       to_varchar(category_id)     as CategoryID,
+       'LoversLightspeed'          as Source
+from ctelightspeedcombined;
 
 
