6370e5b27d3acbaba5b6ca552ad004b7bd3f5f38
stevencyrway <stevencyrway@gmail.com>
2021-09-23 13:14:50 -0500

    item table finished, date dimension table added for reference.

--- ItemTableBuild.sql
+++ ItemTableBuild.sql
@@ -2,29 +2,32 @@ use warehouse COMPUTE_WH;
 -- use database PROD_TLA_DW
 
 --To create the table
--- CREATE OR REPLACE TABLE PROD_TLA_DW.public.ITEM_DIM
--- (
---     SKU         VARCHAR(255) NULL,
---     UUID        VARCHAR(255) NOT NULL,
---     DESCRIPTION VARCHAR(255) NULL,
---     ATTRIBUTE1  VARCHAR(255) NULL,
---     ATTRIBUTE2  VARCHAR(255) NULL,
---     ATTRIBUTE3  VARCHAR(255) NULL,
---     SOURCE      VARCHAR(100) NULL
--- )
---
--- As
+CREATE OR REPLACE TABLE FIVETRAN_DB.PROD.ITEM_DIM
+(
+    SKU         VARCHAR(100) NULL,
+    UUID        VARCHAR(255) NOT NULL,
+    BRANDNAME   VARCHAR(255) NULL,
+    VENDORID    VARCHAR(255) NULL,
+    CATEGORYID  VARCHAR(100)  NULL,
+    DESCRIPTION VARCHAR(255) NULL,
+    COLOR       VARCHAR(100)  NULL,
+    SIZE  VARCHAR(255) NULL,
+    ATTRIBUTE3  VARCHAR(255) NULL,
+    SOURCE      VARCHAR(30) NULL
+)
 
+AS
 --Inventory Table Build
 -- !! Need to confirm timezones of each data that's landed to ensure proper time zone offset.
 WITH RECURSIVE
 ---- /// Lightspeed /// ----
    -- Assigns row numbers to to get values over time and identify when more than value occurs in a day.
     ctelightspeedItem AS (Select ID,
+                                 concat('Lightspeed', '/', CUSTOM_SKU)                          as UUID,
                                  CUSTOM_SKU,
-                                 MANUFACTURER_ID                                                as ManufacturerID,
+                                 to_varchar(MANUFACTURER_ID)                                    as ManufacturerID,
                                  DEFAULT_VENDOR_ID                                              as VendorID,
-                                 to_varchar(CATEGORY_ID) as CategoryID,
+                                 to_varchar(CATEGORY_ID)                                        as CategoryID,
                                  DESCRIPTION,
                                  ATTRIBUTE_1,
                                  ATTRIBUTE_2,
@@ -33,6 +36,10 @@ WITH RECURSIVE
                           FROM FIVETRAN_DB.LIGHT_SPEED_RETAIL.ITEM_HISTORY)
 ---- /// YANDY /// ----
    -- Assigns row numbers to multiple inventory moves per day
+   , cteyandybrands as (
+    Select BRAND_CODE, BRAND_NAME, PROD_ID
+    from POSTGRES_PUBLIC.BRANDS
+             join POSTGRES_PUBLIC.PRODUCTS on BRAND_ID = PROD_BRAND)
    , cteyandycategoryjoin as (Select PROD_ID,
                                      concat(PTYPE, '_', N2) as CategoryID
                               from FIVETRAN_DB.POSTGRES_PUBLIC.PRODUCTS)
@@ -41,11 +48,14 @@ WITH RECURSIVE
                                  PROD_OPTION_ID,
                                  po.option_sku,
                                  COLOR_NAME,
+                                 BRAND_NAME,
+                                 BRAND_CODE,
                                  OPTION_SIZE,
                                  OPTION_STYLE,
                                  SIZES_TABLE_ID
                           from FIVETRAN_DB.POSTGRES_PUBLIC.product_options po
-                                   join POSTGRES_PUBLIC.PRODUCT_COLORS on po.PROD_ID = PRODUCT_COLORS.PROD_ID)
+                                   join POSTGRES_PUBLIC.PRODUCT_COLORS on po.PROD_ID = PRODUCT_COLORS.PROD_ID
+                                   join cteyandybrands on po.PROD_ID = cteyandybrands.PROD_ID)
    , cteyandyProductSizes as (select SIZE_TYPE_ID, SIZE_TYPE, SIZE_NAME, SIZE_VALUE
                               from FIVETRAN_DB.POSTGRES_PUBLIC.SIZES_TABLE)
    , cteyandyProductSizes as (select option_color,
@@ -57,6 +67,7 @@ WITH RECURSIVE
                                           prod_option_id,
                                           option_sku,
                                           COLOR_NAME,
+                                          BRAND_NAME,
                                           option_size,
                                           option_style,
                                           sizes_table_id,
@@ -73,10 +84,9 @@ WITH RECURSIVE
 -- the intent here is to take all the relevant cte's above and combine them through unions to make one conjoined inventory table.
 -- This methodology will be followed for all tables.
 -- Yandy Item's
-Select NUll       as Product,
-       OPTION_SKU as SKU,
+Select OPTION_SKU as SKU,
        UUID       as UUID,
-       null       as ManufacturerID,
+       BRAND_NAME as BrandID,
        null       as VendorID,
        CategoryID as CategoryID,
        null       as Description,
@@ -88,12 +98,11 @@ from cteyandyinventorycombined
 
 union all
 -- Lightspeed Item's
-Select null           as Product,
-       CUSTOM_SKU     as SKU,
-       null           as UUID,
-       ManufacturerID as ManufacturerID,
+Select CUSTOM_SKU     as SKU,
+       UUID           as UUID,
+       ManufacturerID as BrandID,
        VendorID       as VendorID,
-       CategoryID   as CategoryID,
+       CategoryID     as CategoryID,
        DESCRIPTION    as Description,
        Attribute_1    as Color,
        Attribute_2    as Size,
@@ -102,4 +111,4 @@ Select null           as Product,
 from ctelightspeedItem;
 
 
-----
+
