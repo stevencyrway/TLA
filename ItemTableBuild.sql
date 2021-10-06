6e4a91103fd17ebde28c73a2c177cb34dd579680
stevencyrway <stevencyrway@gmail.com>
2021-10-06 15:30:19 -0500

    Item Table Audit finished and ready for prod. Still building tableau query out.

--- ItemTableBuild.sql
+++ ItemTableBuild.sql
@@ -3,15 +3,14 @@ use warehouse COMPUTE_WH;
 
 --To create the table
 CREATE OR REPLACE TABLE FIVETRAN_DB.PROD.ITEM_DIM
-(
-    SKU         VARCHAR(100) NULL,
+(   SKU         VARCHAR(100) NULL,
     UUID        VARCHAR(255) NOT NULL,
     BRANDNAME   VARCHAR(255) NULL,
     VENDORID    VARCHAR(255) NULL,
     CATEGORYID  VARCHAR(100)  NULL,
     DESCRIPTION VARCHAR(255) NULL,
     COLOR       VARCHAR(100)  NULL,
-    SIZE  VARCHAR(255) NULL,
+    SIZE        VARCHAR(255) NULL,
     ATTRIBUTE3  VARCHAR(255) NULL,
     SOURCE      VARCHAR(30) NULL
 )
@@ -23,7 +22,7 @@ WITH RECURSIVE
 ---- /// Lightspeed /// ----
    -- Assigns row numbers to to get values over time and identify when more than value occurs in a day.
     ctelightspeedItem AS (Select ID,
-                                 concat('Lightspeed', '/', ID)                          as UUID,
+                                 concat('Lightspeed', '/', to_varchar(ID))                          as UUID,
                                  CUSTOM_SKU,
                                  to_varchar(MANUFACTURER_ID)                                    as ManufacturerID,
                                  DEFAULT_VENDOR_ID                                              as VendorID,
@@ -36,10 +35,12 @@ WITH RECURSIVE
                           FROM FIVETRAN_DB.LIGHT_SPEED_RETAIL.ITEM_HISTORY)
 ---- /// YANDY /// ----
    -- Assigns row numbers to multiple inventory moves per day
-   , cteyandybrands as (
-    Select BRAND_CODE, BRAND_NAME, PROD_ID
-    from POSTGRES_PUBLIC.BRANDS
-             join POSTGRES_PUBLIC.PRODUCTS on BRAND_ID = PROD_BRAND)
+    , cteyandyproductname as (select PROD_ID, PROD_NAME
+                            from FIVETRAN_DB.POSTGRES_PUBLIC.ORDERS_PRODS
+                            group by PROD_ID, PROD_NAME)
+   , cteyandybrands as (Select BRAND_CODE, BRAND_NAME, PROD_ID, PROD_SKU
+                        from POSTGRES_PUBLIC.BRANDS
+                                 join POSTGRES_PUBLIC.PRODUCTS on BRAND_ID = PROD_BRAND)
    , cteyandycategoryjoin as (Select PROD_ID,
                                      concat(PTYPE, '_', N2) as CategoryID
                               from FIVETRAN_DB.POSTGRES_PUBLIC.PRODUCTS)
@@ -47,56 +48,58 @@ WITH RECURSIVE
                                  po.PROD_ID,
                                  PROD_OPTION_ID,
                                  po.option_sku,
-                                 COLOR_NAME,
+                                OPTION_COLOR,
                                  BRAND_NAME,
                                  BRAND_CODE,
                                  OPTION_SIZE,
                                  OPTION_STYLE,
-                                 SIZES_TABLE_ID
+                                 SIZES_TABLE_ID,
+                                 PROD_SKU,
+                                    PN.PROD_NAME as ProductName
                           from FIVETRAN_DB.POSTGRES_PUBLIC.product_options po
-                                   join POSTGRES_PUBLIC.PRODUCT_COLORS on po.PROD_ID = PRODUCT_COLORS.PROD_ID
-                                   join cteyandybrands on po.PROD_ID = cteyandybrands.PROD_ID)
-   , cteyandyProductSizes as (select SIZE_TYPE_ID, SIZE_TYPE, SIZE_NAME, SIZE_VALUE
-                              from FIVETRAN_DB.POSTGRES_PUBLIC.SIZES_TABLE)
-   , cteyandyProductSizes as (select option_color,
-                                     cgid,
-                                     main_color_id
-                              from FIVETRAN_DB.POSTGRES_PUBLIC.COLOR_GROUPS)
+                                   join cteyandybrands B on po.PROD_ID = b.PROD_ID
+                                    join cteyandyproductname PN on po.PROD_ID = PN.PROD_ID
+                            and IS_AMAZON_PRODUCT is null)
+   , cteyandyProductSizes as (select SIZE_TYPE_ID, SIZE_TYPE
+                              from FIVETRAN_DB.POSTGRES_PUBLIC.SIZES_TABLE
+                            group by SIZE_TYPE_ID, SIZE_TYPE)
+   , cteyandyProductcolors as (select prod_color_id, color_name, prod_id
+                               from POSTGRES_PUBLIC.PRODUCT_COLORS)
    , cteyandyinventorycombined as (Select uuid,
                                           cteYandyProducts.prod_id,
                                           prod_option_id,
-                                          option_sku,
-                                          COLOR_NAME,
+                                          concat(PROD_SKU,'/',OPTION_SKU) as SKU,
+                                        ProductName,
                                           BRAND_NAME,
                                           option_size,
                                           option_style,
                                           sizes_table_id,
                                           categoryid,
-                                          size_type_id,
-                                          size_type,
-                                          size_name,
-                                          size_value
+                                          COLOR_NAME,
+                                          SIZE_TYPE
                                    from cteYandyProducts
                                             join cteyandycategoryjoin
                                                  on cteYandyProducts.PROD_ID = cteyandycategoryjoin.PROD_ID
-                                            left outer join cteyandyProductSizes on SIZES_TABLE_ID = SIZE_TYPE_ID)
+                                            join cteyandyProductcolors on cteYandyProducts.OPTION_COLOR = cteyandyProductcolors.PROD_COLOR_ID
+                                            join cteyandyProductSizes on SIZES_TABLE_ID = SIZE_TYPE_ID)
 ---//// WHERE IT ALL COMBINES ////---
 -- the intent here is to take all the relevant cte's above and combine them through unions to make one conjoined inventory table.
 -- This methodology will be followed for all tables.
 -- Yandy Item's
-Select OPTION_SKU as SKU,
-       UUID       as UUID,
-       BRAND_NAME as BrandID,
-       null       as VendorID,
-       CategoryID as CategoryID,
-       null       as Description,
-       COLOR_NAME as Color,
-       SIZE_VALUE as Size,
-       SIZE_NAME  as Attribute3,
-       'Yandy'    as Source
+Select sku         as SKU,
+       UUID        as UUID,
+       BRAND_NAME  as BrandID,
+       null        as VendorID,
+       CategoryID  as CategoryID,
+       ProductName as Description,
+       COLOR_NAME  as Color,
+       SIZE_TYPE   as Size,
+       null        as Attribute3,
+       'Yandy'     as Source
 from cteyandyinventorycombined
 
 union all
+
 -- Lightspeed Item's
 Select CUSTOM_SKU     as SKU,
        UUID           as UUID,
@@ -108,7 +111,5 @@ Select CUSTOM_SKU     as SKU,
        Attribute_2    as Size,
        Attribute_3    as Attribute3,
        'Lightspeed'   as Source
-from ctelightspeedItem;
-
-
-
+from ctelightspeedItem
+where RowNumber = 1;
--- TableauDashboardQueries.sql
+++ TableauDashboardQueries.sql
@@ -1,6 +1,40 @@
 use warehouse COMPUTE_WH;
 
-Select * from FIVETRAN_DB.PROD.INVENTORY_FACT;
+---concat('Lightspeed', '/', ITEM_ID)  as UUID,
+---concat('Yandy','/',to_varchar(PROD_ID), '/', to_varchar(PROD_OPTION_ID)) as UUID,
 
-Select * from FIVETRAN_DB.PROD.ITEM_DIM
-where source = 'Lightspeed'
+---testing to get it populate inventory for all dates
+WITH RECURSIVE
+---- /// Tableau View /// Assigns Previous date value for comparative join to date table.
+--                        Allows inventory value to populate for applicable dates since last update in source.
+cteinventoryDateLead as (Select *,
+                                Lead(if.DATE) OVER (ORDER BY if.DATE desc) as PreviousRecordDate
+                         from FIVETRAN_DB.prod.INVENTORY_FACT IF
+                         where if.date >= '2021-09-01'
+                           and if.UUID = 'Lightspeed/3862'
+                            and LOCATIONID = '35')
+        ,
+cteinventoryLeadDayAdd as (Select date,
+                                  sku,
+                                  uuid,
+                                  qoh,
+                                  backorder,
+                                  cost,
+                                  itemid,
+                                  locationid,
+                                  categoryid,
+                                  source,
+                                  previousrecorddate,
+                                  dateadd(day,1,PreviousRecordDate) as StartRecordDate
+                           from cteinventoryDateLead)
+
+---- /// Final Table for Tableau /// ----
+
+Select *
+from FIVETRAN_DB.prod.DATE_DIMENSION DD
+        join cteinventoryLeadDayAdd LDA
+                         on DD.DATE >= LDA.StartRecordDate
+                        and DD.Date <= lda.DATE
+        join FIVETRAN_DB.PROD.ITEM_DIM ID
+        on lda.UUID = id.UUID;
+
