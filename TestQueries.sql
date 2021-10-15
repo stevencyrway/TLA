c7d3679e4263dcbbdff67c29d8a269d7bd3b7cfc
stevencyrway <stevencyrway@gmail.com>
2021-10-14 20:40:19 -0500

    Inventory Table is ready.

--- TestQueries.sql
+++ TestQueries.sql
@@ -3,19 +3,20 @@ with recursive
                                 last_day(to_date(to_timestamp(STAMPED)))                                              as MonthEnding,
                                 avg(cost_price)                                                                       as MonthlyCostAvg,
                                 Lead(last_day(to_date(to_timestamp(STAMPED))))
-                                     OVER (ORDER BY FL.prod_option_id, last_day(to_date(to_timestamp(STAMPED))) desc) as PreviousMonthEnding
+                                     OVER (ORDER BY last_day(to_date(to_timestamp(STAMPED))) desc, FL.prod_option_id desc)  as PreviousMonthEnding
                          from FIVETRAN_DB.POSTGRES_PUBLIC.FIFO_LEDGER FL
-                         group by last_day(to_date(to_timestamp(STAMPED))), to_timestamp(stamped), prod_option_id)
+                         group by last_day(to_date(to_timestamp(STAMPED))), prod_option_id)
    , cteyandyinvcostLeadDayAdd as (Select *,
                                        case
-                                           when PreviousMonthEnding >= MonthEnding then MonthEnding
-                                           when PreviousMonthEnding < MonthEnding
-                                               then dateadd(month, 1, PreviousMonthEnding) end as StartMonth
+                                           when PreviousMonthEnding >= MonthEnding then PreviousMonthEnding
+                                           when PreviousMonthEnding < MonthEnding then dateadd(day, 1, PreviousMonthEnding) end as StartMonth
        from cteyandyavgcost)
 
 
-select * from cteyandyavgcost
+select * from cteyandyinvcostLeadDayAdd
 where PROD_OPTION_ID = 176
+
+
 -- fifo_ledger.stamped (int) column is the epoch timestamp of the date/time of the inventory bucket.
 -- to_timestamp(stamped) would get you the datetime translation.
 
@@ -31,3 +32,7 @@ union all
 
 select 'shopify' as source, last_day(DONE), cast(avg(ROWS_UPDATED_OR_INSERTED) as bigint) from LOVERS_SHOPIFY.FIVETRAN_AUDIT
 group by last_day(DONE)
+
+
+Select *, to_timestamp(STAMPED) from FIVETRAN_DB.POSTGRES_PUBLIC.FIFO_LEDGER
+where PROD_OPTION_ID = 176
