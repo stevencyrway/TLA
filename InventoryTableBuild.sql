d2b96f75f3fa496c536a9ae15f1fbaf17d40e729
stevencyrway <stevencyrway@gmail.com>
2021-10-14 22:47:31 -0500

    added the new calculation for the yandy inventory cost in the past. Solved the Yandy prod name causing duplication.

--- InventoryTableBuild.sql
+++ InventoryTableBuild.sql
@@ -32,9 +32,6 @@ WITH RECURSIVE
                                from FIVETRAN_DB.PROD.LIGHTSPEED_INVENTORY_HISTORY)
 ---- /// YANDY /// ----
    -- Assigns row numbers to multiple inventory moves per day
-   , cteyandyproductname as (select PROD_ID, PROD_NAME
-                             from FIVETRAN_DB.POSTGRES_PUBLIC.ORDERS_PRODS
-                             group by PROD_ID, PROD_NAME)
    , cteyandycategoryjoin as (Select PROD_ID,
                                      PRODUCTS.ptype,
                                      PRODUCTS.n2,
@@ -56,15 +53,17 @@ WITH RECURSIVE
                                 last_day(to_date(to_timestamp(STAMPED)))                                              as MonthEnding,
                                 avg(cost_price)                                                                       as MonthlyCostAvg,
                                 Lead(last_day(to_date(to_timestamp(STAMPED))))
-                                     OVER (ORDER BY FL.prod_option_id, last_day(to_date(to_timestamp(STAMPED))) desc) as PreviousMonthEnding
+                                     OVER (ORDER BY last_day(to_date(to_timestamp(STAMPED))) desc, FL.prod_option_id desc)  as PreviousMonthEnding
                          from FIVETRAN_DB.POSTGRES_PUBLIC.FIFO_LEDGER FL
-                         group by last_day(to_date(to_timestamp(STAMPED))), to_timestamp(stamped), prod_option_id)
-   , cteyandyinvcostLeadDayAdd as (Select *,
-                                       case
-                                           when PreviousMonthEnding >= date then date
-                                           when PreviousMonthEnding < date
-                                               then dateadd(month, 1, PreviousMonthEnding) end as StartRecordDate
-                                from cteyandyavgcost)
+                         group by last_day(to_date(to_timestamp(STAMPED))), prod_option_id)
+   , cteyandyinvcostLeadDayAdd as (Select prod_option_id,
+                                          Monthending,
+                                          MonthlyCostAvg,
+                                          case
+                                              when PreviousMonthEnding >= MonthEnding then PreviousMonthEnding
+                                              when PreviousMonthEnding < MonthEnding
+                                                  then dateadd(day, 1, PreviousMonthEnding) end as StartMonth
+                                   from cteyandyavgcost)
    , cteyandyinventorycombined as (Select inventorydate,
                                           option_id,
                                           qoh,
@@ -74,38 +73,38 @@ WITH RECURSIVE
                                           cteYandyProducts.prod_option_id,
                                           CategoryID,
                                           option_sku,
-                                          PROD_NAME,
-                                          cteyandyavgcost.MonthlyCostAvg
+                                        pss.PROD_NAME,
+                                          cteyandyinvcostLeadDayAdd.MonthlyCostAvg
                                    from cteYandyInventoryHistoryPrep
                                             join cteYandyProducts on PROD_OPTION_ID = OPTION_ID
-                                            join cteyandyavgcost
-                                                 on cteyandyavgcost.PROD_OPTION_ID = cteYandyProducts.PROD_OPTION_ID
-                                                and InventoryDate <= MonthEnding
-                                       and InventoryDate >= MonthEnding
+                                            join cteyandyinvcostLeadDayAdd
+                                                 on cteyandyinvcostLeadDayAdd.PROD_OPTION_ID = cteYandyProducts.PROD_OPTION_ID
+                                                and InventoryDate <= cteyandyinvcostLeadDayAdd.MonthEnding
+                                                and inventorydate >= cteyandyinvcostLeadDayAdd.StartMonth
                                             join cteyandycategoryjoin
                                                  on cteYandyProducts.PROD_ID = cteyandycategoryjoin.PROD_ID
-                                            join cteyandyproductname
-                                                 on cteYandyProducts.PROD_ID = cteyandyproductname.PROD_ID ---Need to confirm this join doesn't duplicate anything still 09/29/2021
+                                          LEFT JOIN FIVETRAN_DB.POSTGRES_PUBLIC.products_site_specific pss
+                                             ON cteYandyProducts.prod_id = pss.prod_id AND pss.site_id = 1
                                    where InventoryRowNumber = 1)
-
 
 ---//// WHERE IT ALL COMBINES ////---
 -- the intent here is to take all the relevant cte's above and combine them through unions to make one conjoined inventory table.
 -- This methodology will be followed for all tables.
---
--- -- Yandy
--- Select inventorydate                                                              as Date,
---        option_sku                                                                 as SKU,
---        concat('Yandy', '/', to_varchar(PROD_ID), '/', to_varchar(PROD_OPTION_ID)) as ItemUUID,
---        QOH,
---        Null                                                                       as Backorder,
---        Cost,
---        null                                                                       as LocationID, --couldn't find shop values in yandy data, need to ask Aras.
---        CategoryID                                                                 as CategoryID,
---        'Yandy'                                                                    as Source
--- from cteyandyinventorycombined
---
--- union all
+
+-- Yandy
+
+Select inventorydate                                                              as Date,
+       option_sku                                                                 as SKU,
+       concat('Yandy', '/', to_varchar(PROD_ID), '/', to_varchar(PROD_OPTION_ID)) as ItemUUID,
+       QOH,
+       Null                                                                       as Backorder,
+       MonthlyCostAvg                                                             as Cost,
+       null                                                                       as LocationID, --couldn't find shop values in yandy data, need to ask Aras.
+       CategoryID                                                                 as CategoryID,
+       'Yandy'                                                                    as Source
+from cteyandyinventorycombined
+
+union all
 
 -- Lightspeed
 Select INSERTDATE                    as Date,
